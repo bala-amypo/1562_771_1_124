307d58eba78daed0a6aeca0b89aaca501ab3caa8
bala-amypo <bala@amypo.in>
2025-12-17 08:48:12 +0000

    User commit

--- src/main/java/com/example/demo/entity/DiversityTarget.java
+++ src/main/java/com/example/demo/entity/DiversityTarget.java
@@ -3,5 +3,8 @@ package com.example.DiversityTarget;
 public classDiversityTarget{
     private long id;
     private Integer targetYear;
-    private ManyToOne DiversityClassification classification
+    private ManyToOne DiversityClassification classification;
+    private Double targetPercentage;
+    private Boolean active = true;
+    
 }
