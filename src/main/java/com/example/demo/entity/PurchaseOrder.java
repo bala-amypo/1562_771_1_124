9d2b4d83aa3c212a8c40ccfba1a31e14be913bac
bala-amypo <bala@amypo.in>
2025-12-19 09:47:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/PurchaseOrder.java
+++ src/main/java/com/example/demo/entity/PurchaseOrder.java
@@ -21,8 +21,68 @@ public class PurchaseOrder {
     private String notes;
 
     @ManyToOne
+    @JoinColumn(name = "supplier_id")
     private Supplier supplier;
 
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private SpendCategory category;
+
+    // -------- GETTERS & SETTERS --------
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPoNumber() {
+        return poNumber;
+    }
+
+    public void setPoNumber(String poNumber) {
+        this.poNumber = poNumber;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDateIssued() {
+        return dateIssued;
+    }
+
+    public void setDateIssued(LocalDate dateIssued) {
+        this.dateIssued = dateIssued;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public SpendCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(SpendCategory category) {
+        this.category = category;
+    }
 }
