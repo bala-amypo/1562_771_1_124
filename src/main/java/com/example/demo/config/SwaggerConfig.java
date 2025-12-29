c22ffaef719a6d34df028f735362d20e8e9ac0cd
bala-amypo <bala@amypo.in>
2025-12-29 06:47:06 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,19 +1,39 @@
 package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI supplierDiversityOpenAPI() {
+    public OpenAPI customOpenAPI() {
+
+        SecurityScheme jwtScheme = new SecurityScheme()
+                .name("Authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
         return new OpenAPI()
-                .info(new Info()
-                        .title("Supplier Diversity Tracker API")
-                        .description("Backend APIs for Supplier Diversity Tracker")
-                        .version("1.0.0"));
+                .servers(List.of(
+                        new Server().url("https://9081.pro604cr.amypo.ai")
+                ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList("BearerAuth")
+                )
+
+                .components(
+                        new Components().addSecuritySchemes(
+                                "BearerAuth", jwtScheme
+                        )
+                );
     }
 }
